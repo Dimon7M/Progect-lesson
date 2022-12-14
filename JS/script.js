212f082607d8b846507d705bdf12a35bf0bab8b2
unknown <super.machuha@ukr.net>
2022-12-14 19:35:29 +0200

    first

---
+++ JS/script.js
@@ -0,0 +1,26 @@
+// "use strict";
+
+// let number = 5;
+// const width = 1;
+// number = 10;
+// console.log(number);
+
+// let obj = {
+//     userName: "John",
+//     age: 25,
+//     isMarried: false,
+// };
+// console.log(obj);
+// userName = 'Vasia';
+// isMarried = true;
+// console.log(obj);
+
+let incr = 11,
+    decr = 10;
+
+incr++; 
+decr--;
+
+
+    console.log(incr);
+    console.log(decr);
